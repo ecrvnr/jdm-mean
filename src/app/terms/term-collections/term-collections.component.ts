18ed7c05e5400727e62fcf95260ec8db27fa18fc
ecrvnr <ecrvnr@gmail.com>
2017-12-14 16:37:59 +0100

    Base code for term collections component

--- src/app/terms/term-collections/term-collections.component.ts
+++ src/app/terms/term-collections/term-collections.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TermService } from '../term.service';
 import { Term } from '../term';
+import { OutRel } from '../outRel';
+import { InRel } from '../inRel';
+import { Entry } from '../entry';
 import { Input } from '@angular/core/src/metadata/directives';
 
 @Component({
@@ -14,20 +17,37 @@ export class TermCollectionsComponent {
   @Input()
   term: Term;
 
+  page: Number;
   pageSize: Number;
+  loading: Boolean;
 
+  entries: Entry[];
+  outRels: OutRel[];
+  inRels: InRel[];
 
   constructor(private termService: TermService) { }
 
-  getEntries(page: Number, pageSize: Number) {
-
+  getEntries() {
+    this.loading = true;
+    this.termService.getEntries(this.term.eid, this.page, this.pageSize).then((entries: Entry[]) => {
+      this.entries = entries;
+      this.loading = false;
+    });
   }
 
   getOutRels(page: Number, pageSize: Number) {
-
+    this.loading = true;
+    this.termService.getOutRels(this.term.eid, this.page, this.pageSize).then((outRels: OutRel[]) => {
+      this.outRels = outRels;
+      this.loading = false;
+    });
   }
 
   getInRels(page: Number, pageSize: Number) {
-
+    this.loading = true;
+    this.termService.getInRels(this.term.eid, this.page, this.pageSize).then((inRels: InRel[]) => {
+      this.inRels = inRels;
+      this.loading = false;
+    });
   }
 }
